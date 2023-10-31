e51abef9db8c32df125c2fe12aabf6311f15793f
jaka <jaka72@gmail.com>
2023-10-31 22:36:43 +0100

    first

---
+++ worpressOLD/wp-content/backuply/backuply_config.php
@@ -0,0 +1,5 @@
+<?php exit();?>
+{
+    "BACKUPLY_KEY": "01733b116d3a206185b97e4110de3f0e3eadff23b80e8a3e9a167d12eb0483cf",
+    "RESTORE_KEY": "74b0f3cc1e0bbf1fcce1991b25cca1f657348cf87716caddf51913a43aac0d9e"
+}
